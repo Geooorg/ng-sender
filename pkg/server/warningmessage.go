de288ed71f53549b3850f4e834dad4293401ccd0
Georg Stach <info@GeorgStach.de>
2022-12-05 14:33:28 +0100

    extract host list

--- pkg/server/warningmessage.go
+++ pkg/server/warningmessage.go
@@ -16,3 +16,17 @@ func (s *Server) SendWarningMessageToAllReceivers(w http.ResponseWriter, r *http
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
+
+func (s *Server) extractTargetHosts() []string {
+
+	var hosts = []string{}
+	for _, r := range s.stationListCache.StationsList.Receivers {
+		if len(r.Hosts) > 0 {
+			for _, host := range r.Hosts {
+				hosts = append(hosts, host.Hostname+host.Port)
+			}
+		}
+	}
+
+	return hosts
+}
